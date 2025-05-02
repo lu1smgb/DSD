3e23ac9db0be81a641fb98031d7feb231d8f6b2d
lu1smgb <lu1smgb.gacc@gmail.com>
2025-05-02 13:26:50 +0000

    P3: Clase Transaccion

---
+++ 2024-2025/P3/Transaccion.java
@@ -0,0 +1,55 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Transaccion {
+    
+    static private DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+    static private int transactionCounter = 0;
+    private int id;
+    private String entidad;
+    private int cantidad;
+    private LocalDateTime fecha;
+
+    public Transaccion(String entidad, int cantidad) {
+        this.id = transactionCounter++;
+        this.entidad = entidad;
+        this.cantidad = cantidad > 0 ? cantidad : 0;
+        this.fecha = LocalDateTime.now();
+    }
+
+    public String getEntidad() {
+        return entidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + entidad + "@" + fecha.format(formatoFecha) + " -> $" + cantidad;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Transaccion)) return false;
+        return Objects.equals(id, ((Transaccion) obj).id);
+    }
+
+    public static void main(String[] args) {
+        Transaccion t = new Transaccion("Luis", 50);
+        System.out.println(t);
+    }
+
+}
